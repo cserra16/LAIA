489f692a6d11b93e5244ead75edf70578b1a0c4f
cserra16 <103030841+cserra16@users.noreply.github.com>
2025-12-29 15:24:24 +0100

    Initial Commit

---
+++ LAIATests/LAIATests.swift
@@ -0,0 +1,17 @@
+//
+//  LAIATests.swift
+//  LAIATests
+//
+//  Created by carlos on 29/12/25.
+//
+
+import Testing
+@testable import LAIA
+
+struct LAIATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
